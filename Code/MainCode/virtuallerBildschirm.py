21342a308e64a25fa96980f298fd13f410375b71
unknown <lamorenoca@unal.edu.co>
2024-04-24 13:36:14 +0200

    the code works

--- Code/MainCode/virtuallerBildschirm.py
+++ Code/MainCode/virtuallerBildschirm.py
@@ -1,16 +1,43 @@
 import serial.tools.list_ports
+import matplotlib.pyplot as plt
+import time
 from graphics import *
-
+plt.ion()
 
 
 def main():
+
+    # Plotting settings
+    start_time=time.time()
+    pressure_array=[1000, 1000]
+    time_array=[0,0.1]
+    graph=plt.plot(time_array, pressure_array)[0]
+    plt.xlabel('Time (s)')
+    plt.ylabel('Pressure (mbar)')
+    plt.title('ITR 090 Sensor Pressure read')
+    ####
+
     screen_state=0
     arrow_state=0
     
     win=GraphWin("Program",500,300)
-    
+    aCircle=Circle(Point(300, (arrow_state+2)*20+10), 5)
+    aCircle.setFill("blue")
+    aCircle.setOutline("blue")
 
     msg=state_machine(0,0,0,0,0,0,0,0)
+    aux_dict=dict(
+        BPStatus='0',
+        TPStatus='0',
+        pressure1='0',
+        pressure2='0',
+        temElec='0',
+        tempMotor='0',
+        tempPmpBot='0',
+        screen_state='0',
+        arrow_state='0'
+    )
+    printed_dict=aux_dict
     for i in msg:
         i.draw(win)
 
@@ -23,34 +50,34 @@ def main():
             received_string=packet.decode('utf').rstrip('\n')
             decoded_string=read_string(received_string)
             print(decoded_string)
-            BPStatus=int(decoded_string['BPStatus']),
-            TPStatus=int(decoded_string['TBStatus']),
-            pressure1=decoded_string['pressure1'],
-            pressure2=decoded_string['pressure2'],
-            temElec=int(decoded_string['temElec']),
-            tempMotor=int(decoded_string['tempMotor']),
-            tempPmpBot=int(decoded_string['tempPmPBot']),
-            screen_state=int(decoded_string['screen_state'])
-            arrow_state=int(decoded_string['arrow_state'])
+            if (decoded_string['BPStatus']=='0')|(decoded_string['BPStatus']=='1'):
+                printed_dict=decoded_string
+                if float(decoded_string['pressure1'])!=0: pressure_array.append(float(decoded_string['pressure1']))
+                else: pressure_array.append(pressure_array[-1])
+                time_array.append(time.time()-start_time)
+                graph.remove()
+                graph=plt.plot(time_array,pressure_array)[0]
         
-        if state_aux!=screen_state:
+        if aux_dict!=printed_dict:
             print("enters")
             for i in msg: i.undraw()
-            msg=state_machine(screen_state, TPStatus, BPStatus, pressure1, pressure2, temElec, tempMotor, tempPmpBot)
+            msg=state_machine(int(printed_dict['screen_state']), 
+                              int(printed_dict['TPStatus']), 
+                              printed_dict['BPStatus'], 
+                              printed_dict['pressure1'], 
+                              printed_dict['pressure2'], 
+                              int(printed_dict['temElec']), 
+                              int(printed_dict['tempMotor']), 
+                              int(printed_dict['tempPmpBot']))
             for i in msg: i.draw(win)
-            if screen_state==1: 
-                aCircle=Circle(Point(300, (arrow_state+2)*20+10), 5)
+            if printed_dict['screen_state']=='1':
+                aCircle.undraw()
+                aCircle=Circle(Point(300, (int(printed_dict['arrow_state'])+2)*20+10), 5)
                 aCircle.setFill("blue")
                 aCircle.setOutline("blue")
                 aCircle.draw(win)
             else: aCircle.undraw()
-            state_aux=screen_state
-        '''
-        key=win.getKey()
-        if (key=="c")|(key=="C"):
-            screen_state=screen_state+1
-            if screen_state==6: break
-        '''
+            aux_dict=printed_dict
 
 
 def read_string(in_string):
@@ -73,19 +100,17 @@ def read_string(in_string):
     print(char_matrix[2][:])
     in_data=dict(
         BPStatus=char_matrix[0][0],
-        TBStatus=char_matrix[1][0],
+        TPStatus=char_matrix[1][0],
         pressure1=''.join(char_matrix[2][:]),
         pressure2=''.join(char_matrix[3][:]),
         temElec=''.join(char_matrix[4][:]),
         tempMotor=''.join(char_matrix[5][:]),
-        tempPmPBot=''.join(char_matrix[6][4:5]),
+        tempPmpBot=''.join(char_matrix[6][4:5]),
         screen_state=char_matrix[7][0],
         arrow_state=char_matrix[8][0]
     )
     return in_data
-    
-    #return char_matrix
-        
+            
 def PressureScreen(TPStatus, BPStatus, pressure1, pressure2):
     msg=[]
     str="Turbo Pump Status: "
@@ -119,23 +144,21 @@ def PressureScreen(TPStatus, BPStatus, pressure1, pressure2):
     str="ITR090: "
     text_cursor=(len(str)/2)*12
     msg.append(Text(Point(text_cursor,130), str))
-    text_cursor=text_cursor+len(str)*6
+    text_cursor=text_cursor+len(str)*10
     msg.append(Text(Point(text_cursor,130), pressure1))
-    text_cursor=text_cursor+len(str)*4.8
+    text_cursor=text_cursor+len(str)*8
     msg.append(Text(Point(text_cursor,130), " mbar"))
     str="RPT010: "
     text_cursor=(len(str)/2)*12
     msg.append(Text(Point(text_cursor,150), str))
-    text_cursor=text_cursor+len(str)*6
+    text_cursor=text_cursor+len(str)*10
     msg.append(Text(Point(text_cursor,150), pressure2))
-    text_cursor=text_cursor+len(str)*4.8
+    text_cursor=text_cursor+len(str)*8
     msg.append(Text(Point(text_cursor,150), " mbar"))
     str="Press OK for settings"
     text_cursor=(len(str)/2)*9
     msg.append(Text(Point(text_cursor,190), str))
     msg[len(msg)-1].setTextColor("blue")
-
-
     return msg
     
 def MenuScreen(TPStatus, BPStatus):
@@ -250,6 +273,7 @@ def state_machine(screen_state, TPStatus, BPStatus, pressure1, pressure2, temEle
             case 5: msg=BastaetigungScreen(TPStatus, 1)
             case _: msg=PressureScreen(TPStatus, BPStatus, pressure1, pressure2)
     return msg
+
 
 ports=serial.tools.list_ports.comports()
 serialInst = serial.Serial()
